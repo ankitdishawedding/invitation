53fde62db232e0bb83a2d616a988a9d42d41b5ce
Ankit Tyagi <ankit.tyagi02@telusinternational.com>
2023-12-23 13:33:26 +0530

    feat: basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Bridegroom from "./components/Bridegroom";
+import Countdown from "./components/Countdown";
+import Footer from "./components/Footer";
+import Gallery from "./components/Gallery";
+import Header from "./components/Header";
+import Organization from "./components/Organization";
+import RSVP from "./components/RSVP";
+import Seeyou from "./components/Seeyou";
+import Sidebar from "./components/Sidebar";
+import Story from "./components/Story";
+import Where from "./components/Where";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Sidebar />
+      <div id="oliven-main">
+        <Header />
+        <Bridegroom />
+        <Countdown />
+        <Organization />
+        <Seeyou />
+        <Where />
+        <Footer />
+      </div>
+    </>
   );
 }
 
